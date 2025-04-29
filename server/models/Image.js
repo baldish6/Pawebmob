83ab3f9744dbf06136149dc83ba570a8de9e15ca
Baldish <baldish6singh@gmail.com>
2025-04-29 23:01:54 +0200

    added models

---
+++ server/models/Image.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const ImageSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: [String],
+      default: [],
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Image", ImageSchema);
