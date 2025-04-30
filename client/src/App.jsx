3b8797707751c791775871ab8053a2d9eced301f
Baldish <baldish6singh@gmail.com>
2025-04-30 12:23:16 +0200

    base react app

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+    </div>
+  )
+}
+
+export default App
