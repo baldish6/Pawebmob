6abd292b42c4c6aaf2141e9d97444b0b149a0444
Baldish <baldish6singh@gmail.com>
2025-06-13 22:40:50 +0200

    fixed commented live url

--- client/src/services/api/BackEndUrl.jsx
+++ client/src/services/api/BackEndUrl.jsx
@@ -1,5 +1,5 @@
 
-//export const LiveUrl = "https://pawebmob-backend.onrender.com/api";
+export const LiveUrl = "https://pawebmob-backend.onrender.com/api";
 //const port = import.meta.env.VITE_PORT;
 //export const LocalUrl = "http://localhost:"+"4000"+"/api";
 //export const LiveUrl = LocalUrl;
