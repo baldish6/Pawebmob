d575b285cc30af354c0e42de92fbd2b58c3ff812
Baldish <baldish6singh@gmail.com>
2025-06-14 11:50:26 +0200

    attempt 11

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -60,7 +60,8 @@ export const login = async (req, res, next) => {
 
     delete req.body.password;
 
-    res.headers.add('Set-Cookie', 'access_token='+token+'; HttpOnly; SameSite=None; Secure; Path=/; Partitioned;')
+
+    res.setHeader('Set-Cookie', 'access_token='+token+'; HttpOnly; SameSite=None; Secure; Path=/; Partitioned;')
 
     //res.cookie("access_token", token, {httpOnly: true});
 
@@ -77,7 +78,7 @@ export const logout = async(req,res)=>{
 
 //res.cookie("access_token", "", {httpOnly: false,maxAge:1 });
 
-res.headers.add('Set-Cookie', 'refreshToken=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0; HttpOnly; SameSite=None; Secure; Path=/; Partitioned;')
+res.setHeader('Set-Cookie', 'access_token=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0; HttpOnly; SameSite=None; Secure; Path=/; Partitioned;')
 
      res.status(200)
       .json({ success: true });
