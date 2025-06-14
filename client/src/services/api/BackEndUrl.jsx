5eb4f9f743f25ca6bb03abaaeee1486f526ee482
Baldish <baldish6singh@gmail.com>
2025-06-14 12:22:31 +0200

    gthb tst 3

--- client/src/services/api/BackEndUrl.jsx
+++ client/src/services/api/BackEndUrl.jsx
@@ -1,5 +1,5 @@
 //export const LiveUrl = "https://pawebmob-backend.onrender.com/api";
-export const LiveUrl = "https://baldish6.github.io/Pawebmob/api"
+export const LiveUrl = "https://github.com/baldish6/Pawebmob/tree/master/api"
 //const port = import.meta.env.VITE_PORT;
 //export const LocalUrl = "http://localhost:"+"4000"+"/api";
 //export const LiveUrl = LocalUrl;
--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({
     //origin:"http://localhost:5173",
    //origin:"https://pawebmob-frontend.onrender.com",
-   origin:"https://baldish6.github.io/Pawebmob/",
+   origin:"https://github.com/baldish6/Pawebmob/tree/master/",
     credentials:true
 }))
 
