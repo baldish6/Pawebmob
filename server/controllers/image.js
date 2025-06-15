b32fd2beec069ffe7f2f38bac17b6cb79d15ee04
Baldish <baldish6singh@gmail.com>
2025-06-16 01:55:26 +0200

    image zod backend errors

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -3,33 +3,21 @@ import Image from "../models/Image.js";
 import { createError } from "../error.js";
 import { z } from "zod" 
 
-const MAX_FILE_SIZE = 500000;
-const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
-export const ImageFileSchema = z.object({
-  image: z
-    .any()
-    .refine((files) => files?.length == 1, "Image is required.")
-    .refine((files) => files?.[0]?.size <= MAX_FILE_SIZE, `Max file size is 5MB.`)
-    .refine(
-      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-      ".jpg, .jpeg, .png and .webp files are accepted."
-    )
-})
 
-const SinglePostSchema = z.object({
+const PostSchema = z.object({
   title:z.string()
   .min(1,"Title is required")
   .max(300,"Max 300 characters"), 
+  imgUrl:z.string(),
   desc:z.string().optional()
 });
 
-export const PostSchema = SinglePostSchema.merge(ImageFileSchema) ;
 
 
 export const addImage = async (req,res,next) => {
 
-  const result = PostSchema.safeParse(req.body);
+  const result = Schema.safeParse(req.body);
     let zodErrors = {};
     if(!result.success){
         result.error.issues.forEach((issue)=>{
